3a8c69626d748e084fcdfb7c5b39270f78794338
Ad5001 <Ad5001@users.noreply.github.com>
2016-08-31 14:35:43 +0300

    Create PMPluginUpdater.php

---
+++ PMPluginUpdater.php
@@ -0,0 +1,11 @@
+<?php
+namespace Author\Plugin;
+
+use pocketmine\utils\Utils;
+/*
+This updater class has been made by the (r) MC-PE.GA All rights reserved.
+This updater has the purpose to update a plugin new version very easily
+Put this into your folder, call it with new Updater($this); and the see the magic !
+*/
+
+class Updater {}
